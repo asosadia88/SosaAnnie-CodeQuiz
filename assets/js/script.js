e32bd5bcfdb3195e06dbc3d9847455a8c11510d5
asosadia88 <annielys.sosa@gmail.com>
2023-08-23 23:07:23 -0400

    saved work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ var startBtn = document.getElementById("start-btn")
 var introSectionEl = document.getElementById("intro-section")
 
 var questionSectionEl = document.getElementById("question-section")
+var initialSectionEl = document.getElementById("initial-section")
 var titleEl = document.getElementById('title')
-var timerEl = document.getElementById('timer')
+var timerEl = document.getElementById('timerParagraph')
+var saveButtonEl = document.getElementById('save-btn')
+var highscoreSectionEl = document.getElementById('highscore-section')
+var clearHistoryEl = document.getElementById('clear-btn')
+var goBackEl = document.getElementById('go-back-bt')
 var choicesEl = document.querySelectorAll(".choices")
 var questionIndex = 0
 var questionsArray = [
     {
-        title: "q 1",
+        title: "What is the capital of India",
         choices: ["c1", "c2", "c3", "c4"],
         answer: "c2"
     },
@@ -33,8 +38,9 @@ var questionsArray = [
         answer: "c4"
     }
 ]
+var resultsArray = [];
 
-var timeLeft = questionsArray.length * 15
+var displayTime = 0;
 
 /*
   1. hide intro section
@@ -45,6 +51,7 @@ var timeLeft = questionsArray.length * 15
 */
 
 var setIntervalId = 0;
+var timeLeft = 0;
 
 function startQuiz() {
     //  introSectionEl.classList.add("hide")
@@ -52,10 +59,11 @@ function startQuiz() {
     questionSectionEl.removeAttribute("class")
     setIntervalId = setInterval(countDown, 1000)
     showQuestions()
+    timeLeft = questionsArray.length * 15
 }
 
 function countDown() {
-    timerEl.textContent = timeLeft--
+    timerEl.textContent = "Time: "+timeLeft--
     if (timeLeft === 0) {
         clearInterval(setIntervalId)
     }
@@ -63,27 +71,85 @@ function countDown() {
 
 function showQuestions() {
     titleEl.textContent = questionsArray[questionIndex].title
-
     choicesEl[0].textContent = questionsArray[questionIndex].choices[0]
-
     choicesEl[1].textContent = questionsArray[questionIndex].choices[1]
-
     choicesEl[2].textContent = questionsArray[questionIndex].choices[2]
-
     choicesEl[3].textContent = questionsArray[questionIndex].choices[3]
-
-
-
 }
 
 function nextQuestion(event) {
     var currentElement = event.target
+    var userChoice = event.target.innerText
+    document.getElementById("answerFeedback").innerHTML = "";
     if (currentElement.matches("button")) {
+        if(userChoice == questionsArray[questionIndex].answer){
+            // display correct message
+            document.getElementById("answerFeedback").innerHTML = "Correct!";
+        } else {
+            // display incorrect message
+            document.getElementById("answerFeedback").innerHTML = "Wrong!";
+            // subtract time from clock
+            timeLeft -= 15
+        }
         questionIndex++
-        showQuestions()
+        if(questionIndex < questionsArray.length){
+            showQuestions()
+        } else {
+            timerEl.setAttribute("class", "hide")
+            questionSectionEl.setAttribute("class", "hide")
+            initialSectionEl.removeAttribute("class")
+            displayTime = timeLeft
+            document.getElementById("score").innerHTML = ""+displayTime;
+        }
     }
+}
+
+function createNode(s, parent){
+    return feedbackText = document.createTextNode(s);
+}
+
+// https://stackoverflow.com/questions/3450593/how-do-i-clear-the-content-of-a-div-using-javascript
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+function saveResult(){
+    var value = document.getElementById('initial-input').value
+    document.getElementById('initial-input').value = ""
+    resultsArray.push({initials: value, result: displayTime})
+    initialSectionEl.setAttribute("class", "hide")
+    highscoreSectionEl.removeAttribute("class")
+    document.getElementById("answerFeedback").innerHTML = ""
+    for(var i=0; i < resultsArray.length; i++){
+        const li = document.createElement('li');
+        const ol = document.querySelector("#highscore-list");
+        const text = document.createTextNode(resultsArray[i].initials+": "+resultsArray[i].result);
+        li.appendChild(text);
+        ol.appendChild(li);
+    }
+}
+
+function clearHistory(){
+    resultsArray = []
+    document.getElementById('highscore-list').innerHTML = ""
+    document.getElementById('answerFeedback').innerHTML = ""
+}
+
+function startOver(){
+    highscoreSectionEl.setAttribute("class", "hide")
+    introSectionEl.removeAttribute("class")
+    timerEl.removeAttribute("class")
+    questionIndex = 0
+    document.getElementById('highscore-list').innerHTML = ""
 }
 
 startBtn.addEventListener("click", startQuiz)
 
 questionSectionEl.addEventListener("click", nextQuestion)
+
+saveButtonEl.addEventListener("click", saveResult)
+
+clearHistoryEl.addEventListener("click", clearHistory)
+
+goBackEl.addEventListener("click", startOver)
